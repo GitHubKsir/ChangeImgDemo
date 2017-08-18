7cbc8df79c1c468196d17fe1cf1a701d8ef4a21f
GitHubKsir <LeadFair@leadfair-2.local>
2017-08-18 15:00:12 +0800

    Initial Commit

---
+++ ChangeImgDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChangeImgDemo
+//
+//  Created by LeadFair on 2017/8/18.
+//  Copyright © 2017年 kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChangeImgDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChangeImgDemo
+//
+//  Created by LeadFair on 2017/8/18.
+//  Copyright © 2017年 kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
